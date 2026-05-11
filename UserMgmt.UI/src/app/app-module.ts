8c75d735bd0c91647cea257f8c2d12f8c3e19bd9
imtrainer2020 <imtrainer2020@gmail.com>
2026-05-11 14:42:49 +0530

    structure change only

--- UserMgmt.UI/src/app/app-module.ts
+++ UserMgmt.UI/src/app/app-module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
-import { LoginComponent } from './features/auth/components/login/login.component';
+import { LoginComponent } from './components/login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SignupComponent } from './components/signup/signup.component';
+import { ForgetPasswordComponent } from './components/forget-password/forget-password.component';
 
 @NgModule({
-  declarations: [App, LoginComponent],
-  imports: [BrowserModule, AppRoutingModule,ReactiveFormsModule],
+  declarations: [App, LoginComponent, SignupComponent, ForgetPasswordComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
 })
--- UserMgmt.UI/src/app/app-routing-module.ts
+++ UserMgmt.UI/src/app/app-routing-module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './features/auth/components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { ForgetPasswordComponent } from './components/forget-password/forget-password.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent }, 
+  { path: 'forgot-password', component: ForgetPasswordComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' } // Defaults to login page
 ];
 
