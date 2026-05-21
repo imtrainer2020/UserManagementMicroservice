7cc69f644492494c8a7f72fd97e00837e968af8d
imtrainer2020 <97571338+imtrainer2020@users.noreply.github.com>
2026-05-21 06:18:01 +0000

    Added Docker support

--- UserMgmt.UI/src/app/models/common.model.ts
+++ UserMgmt.UI/src/app/models/common.model.ts
@@ -1,3 +1,7 @@
+// export class Common {
+//   API_BASE_URL: string = 'http://localhost:5070/gateway/';
+// }
 export class Common {
-  API_BASE_URL: string = 'http://localhost:5070/gateway/';
+  // Use your exact copied URL for port 5070. Make sure it has https:// and ends with /gateway/
+  API_BASE_URL: string = 'https://scaling-giggle-xgr4jpj7947f9jr-5070.app.github.dev/gateway/';
 }
